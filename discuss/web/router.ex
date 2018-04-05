3269e9df68498e02bd842e599587f9c4c74e6613
Brendon Barreto Ribeiro <brendon@meuspedidos.com.br>
2018-04-05 13:24:11 -0300

    oauth

--- discuss/web/router.ex
+++ discuss/web/router.ex
@@ -21,6 +21,14 @@ defmodule Discuss.Router do
     post "/topics", TopicController, :create
     get "/topics/:id/edit", TopicController, :edit
     put "/topics/:id", TopicController, :update
+    delete "/topics/:id", TopicController, :delete
+  end
+
+  scope "/auth", Discuss do
+    pipe_through :browser
+
+    get "/:provider", AuthController, :request
+    get "/:provider/callback", AuthController, :callback
   end
 
   # Other scopes may use custom stacks.
